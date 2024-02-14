84323efbcd30af8dca1742db8c3a11bdd0263435
Raissa Lauar Navarro <raissalgns@gmail.com>
2024-02-14 14:02:58 -0300

    primeiro projeto
    leitura e escrita no console

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade: ");
+            UInt16 idade = Convert.ToUInt16(Console.ReadLine());
+
+            Console.WriteLine("Informe seu gênero. Pressione F para feminino e M para masculino. ");
+            string genero = Console.ReadKey(true).KeyChar.ToString();
+
+            // faz o programa pular a linha
+            Console.WriteLine("\r\n");
+
+
+            Console.WriteLine("Olá " + nome + " !");
+            Console.WriteLine("Você tem " +  idade + " anos de idade");
+            switch (genero)
+            {
+                case "F":
+                    Console.WriteLine("E seu gênero é feminino");
+                    break;
+                case "M":
+                    Console.WriteLine("E seu gênero é masculino");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
